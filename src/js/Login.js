cd53f54e4a8a94d82647433c722c4d1bed0f2755
Álefe Lucas <alefelucas.contato@gmail.com>
2019-11-21 22:24:59 -0300

    Ajustado a aparencia campos de texto, fundo e logo da ioasys. Ainda é necessário adicionar responsividade.

--- src/js/Login.js
+++ src/js/Login.js
@@ -4,52 +4,59 @@ import sith from '../images/login/sith-logo.png';
 import cacadores from '../images/login/cacadores-logo.png';
 import jedi from '../images/login/jedi-logo.png';
 import '../css/Login.css';
+import userIcon from '../images/login/iconuser.svg';
+import keyIcon from '../images/login/iconkey.svg';
+
 
 function Login() {
-  return (
-    <div className="container">
-        <img className="logo" src={ioasys} alt="Logo iOasys" />
-        <div className="clear"></div>
-        <div className="login">
-            <div className="caixa-login">
-                <div className="cabecalho-login">
-                    <div >
-                        <h2>Login</h2>
+    return (
+        <div className="container">
+            <img className="logo"  id="logo-ioasys" src={ioasys} alt="Logo iOasys"/>
+            <div className="clear"></div>
+            <div className="login">
+                <div className="caixa-login">
+                    <div className="cabecalho-login">
+                        <div>
+                            <h2>Login</h2>
+                        </div>
+                        <div>
+                            <img className="imagens-login" src={jedi} alt="Logo sith"/>
+                            <img className="imagens-login" src={sith} alt="Logo sith"/>
+                            <img className="imagens-login-cacadores" src={cacadores} alt="Logo sith"/>
+                        </div>
                     </div>
-                    <div>
-                        <img className="imagens-login" src={jedi} alt="Logo sith" />
-                        <img className="imagens-login" src={sith} alt="Logo sith" />
-                        <img className="imagens-login-cacadores" src={cacadores} alt="Logo sith" />
-                    </div>
+                    <form className="form-login">
+                        <div className="area-login">
+
+                            <img src={userIcon} className="form-icon"/>
+                            <input className="area-text-login" type="text" name="email"
+                                   placeholder="Username/Email"/>
+                        </div>
+                        <div className="area-login">
+                            <img src={keyIcon} className="form-icon"/>
+
+                            <input id="password-text" className="area-text-login" type="password" name="senha"
+                                   placeholder="Senha"/>
+                        </div>
+
+                        < input id="botao-login" type="button" value="Entrar"/>
+                    </form>
+
+
                 </div>
-                <form className="form-login">
-                    <div className="area-login">
-                        <span><ul className="mensagem-erro"></ul></span>
-                        <label for="email"></label>
-                        <input className="area-text-login" type="text" name="email" placeholder="E-mail / Username"/>
-                    </div>
-                    <div className="area-login">
-                        <label for="senha"></label>
-                        <input className="area-text-login" type="password" name="senha" placeholder="Senha"/>
-                    </div>
-                    < input id="botao-login" type="button" value="Entrar"/>
-                </form>
-
-
-            </div>
-            <div className="possibilidades-login">
+                <div className="possibilidades-login">
                     <span>
                         <a href="cadastrar.html">Cadastrar</a>
                     </span>
 
-                <span>
+                    <span>
                         <a href="cadastrar.html">Esqueci minha senha</a>
                     </span>
-            </div>
+                </div>
 
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Login;
