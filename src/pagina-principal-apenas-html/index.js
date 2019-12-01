10f2ef3dd1f14a692572af0e7b7dea06e7efe989
Couth <craftcouth@gmail.com>
2019-12-01 09:26:48 -0300

    Responsividade mobile e correção de bug
    Adicionado a responsividade da versão mobile
    Corrigido um bug do modal do perfil

--- src/pagina-principal-apenas-html/index.js
+++ src/pagina-principal-apenas-html/index.js
@@ -2,24 +2,34 @@ var collapsed = false;
 var searchBar = document.getElementById("search");
 var modal = document.getElementById("profile-modal");
 var profile = document.getElementById("profile-section");
+var floatCollapse = document.getElementById("float-collapse-image");
 
-function collapseSidebar() {
+floatCollapse.onclick = function collapseSidebarDesktop() {
   if (collapsed) {
     document.getElementById("sidebar").style.width = "325px";
     document.getElementById("header").style.marginLeft = "325px";
     document.getElementById("content").style.marginLeft = "325px";
     profile.style.padding = "25px";
     document.getElementById("float-collapse").style.marginLeft = "305px";
-    document.getElementById("float-collapse-image").src = "images/icon-material-keyboard-arrow-left.png";
-    document.getElementById("float-collapse-image").alt = "Imagem de seta para a esquerda";
+    floatCollapse.src = "images/icon-material-keyboard-arrow-left.png";
+    floatCollapse.alt = "Imagem de seta para a esquerda";
   } else {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("header").style.marginLeft = "0";
     document.getElementById("content").style.marginLeft = "0";
     profile.style.padding = "0";
     document.getElementById("float-collapse").style.marginLeft = "0";
-    document.getElementById("float-collapse-image").src = "images/icon-material-keyboard-arrow-right.png";
-    document.getElementById("float-collapse-image").alt = "Imagem de seta para a direita";
+    floatCollapse.src = "images/icon-material-keyboard-arrow-right.png";
+    floatCollapse.alt = "Imagem de seta para a direita";
+  }
+  collapsed = !collapsed;
+}
+
+document.getElementById("profile-mobile").onclick = function collapseSideBarMobile() {
+  if (collapsed) {
+    document.getElementById("sidebar").style.height = "80px";
+  } else {
+    document.getElementById("sidebar").style.height = "100%";
   }
   collapsed = !collapsed;
 }
@@ -35,17 +45,17 @@ window.onscroll = function() {
   }
 }
 
-profile.onclick = function() {
-  modal.style.marginTop = "0";
-}
-
 window.onclick = function(e) {
-  if (profile.contains(e.target)) {
-    if (this.modal.style.marginTop == "-400px") {
-      this.modal.style.marginTop = "0px";
-    }
-  } else if (!modal.contains(e.target)) {
-    if (this.modal.style.marginTop == "0px") {
+  if (!modal.contains(e.target)) {
+    if (profile.contains(e.target)) {
+      if (this.modal.style.marginTop == "-400px" || this.modal.style.marginTop == "") {
+        this.modal.style.marginTop = "0px";
+      } else {
+        if (!document.getElementById("profile-mobile").contains(e.target)) {
+          this.modal.style.marginTop = "-400px";
+        }
+      }
+    } else {
       this.modal.style.marginTop = "-400px";
     }
   }
